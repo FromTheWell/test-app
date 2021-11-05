d41af7f34afe933665b99a2c149af80fc16e1853
Lucas <lucas23delp@gmail.com>
2021-11-05 14:14:17 +0100

    Creada tabla (ahora se muestran los datos)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TablaComponent } from './components/tabla/tabla.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TablaModule } from './components/tabla/tabla.module';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    TablaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    TablaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/tabla/tabla.component.ts
+++ src/app/components/tabla/tabla.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import testSanitas from '../../../assets/testSanitas.json';
 
 @Component({
   selector: 'app-tabla',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TablaComponent implements OnInit {
 
+
+
+  public photoList: Photo[] = testSanitas;
+
+  public displayedColumns: string[] = ['_id', 'photo', 'text'];
+
+  /** Contiene los registros que se muestran en la tabla*/
+  public dataSource: MatTableDataSource<any> = new MatTableDataSource(this.photoList);
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  /** Aplica el filtro del a búsqueda al datasource */
+  public applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // Datasource defaults to lowercase matches
+    this.dataSource.filter = filterValue;
+}
+
+}
+
+export interface Photo {
+  _id: string;
+  photo: string;
+  text: string
 }
